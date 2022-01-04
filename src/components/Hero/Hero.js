d80fd15025a8fa981ae8a0dd5ab7842ed1e50438
Rashedul Islame <rasheduzzaman521@gmail.comm>
2022-01-04 23:42:47 +0600

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,34 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Hero.css";
+
 import Logo from "../../Images/logo/Namlipi.png";
 import google from "../../Images/logo/google.png";
 import facebook from "../../Images/logo/facebook.jpg";
-import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
 
-const Hero = ({ placeholder, data }) => {
-  const [filteredData, setFilteredData] = useState([]);
-  const [wordEntered, setWordEntered] = useState("");
-  console.log(data)
-  const handleFilter = e => {
-    const searchWord = e.target.value;
-    setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
-
-    if (searchWord === "") {
-      setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
-  };
-
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
-  
+const Hero = () => {
   return (
     <div className="container mt-3">
       <div className="d-flex justify-content-between align-items-center">
@@ -36,61 +13,29 @@ const Hero = ({ placeholder, data }) => {
           <img src={Logo} alt="" />
         </div>
         <div className="search_input">
-
-              {/* <label
+          <form>
+            <div className="m-auto">
+              <label
                 className="visually-hidden"
                 htmlFor="inlineFormInputGroupUsername"
               >
                 Username
-              </label> */}
-              {/* you need work here some reponsive 
-              below  i just copy my code*/}
-
-
-              <div className="search">
-                <div className="searchInputs">
-                  <input
-                    type="text"
-                    placeholder={placeholder}
-                    value={wordEntered}
-                    onChange={handleFilter}
-                  />
-                  <div className="searchIcon">
-                    {filteredData.length === 0 ? (
-                      <SearchIcon />
-                    ) : (
-                      <CloseIcon
-                      id="clearBtn" onClick={clearInput} />
-                    )}
-                  </div>
-                </div>
-                    {filteredData.length !== 0 && (
-                  <div className="dataResult">
-                    {filteredData.slice(0, 15).map((value, key) => {
-                      return (
-                        <a className="dataItem" href={value.link} target="_blank" rel='noreferrer'>
-                          <p>{value.author} </p>
-                        </a>
-                      );
-                    })}
-                  </div>
-                  )}
-    
-                    {/* <div className="input-group">
-                    <input
+              </label>
+              <div className="input-group">
+                <input
                   type="text"
                   className="form-control input_area"
                   id="inlineFormInputGroupUsername"
-                  placeholder={placeholder}
-                    />
-                     <div className="input-group-text">
+                  placeholder="Username"
+                />
+                <div className="input-group-text">
                   <div className="icon">Search</div>
-                    </div>
-                    </div> */}
+                </div>
               </div>
-      
-           </div>
-           <div className="login">
+            </div>
+          </form>
+        </div>
+        <div className="login">
           <div className="hover_login">
             <i className="fas fa-cart-plus  fs-5 me-2 me-4 addToCard"></i>
             <button className="btn border ">Login</button>
